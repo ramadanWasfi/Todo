b268ada72009e4c9c2166671ec4a5e21891196c7
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2024-08-19 23:44:09 +0300

    Test Functions in index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import {Task} from "./Task";
+import { Todo } from "./Todo";
 
-const task = Task();
+const task = Task("task A", 'aa',2);
+const task2 = Task("task b", 'd', 1);
+task.setState("finished");
+
+const todo = Todo("a","b",[task, task2]);
+
+let a = todo.getTaskByPriorityNumber(2);
+let b = todo.getTaskByPriorityNumber(1);
+let c = todo.getTaskByState("finished");
+
+console.log(a.getName());
+console.log(b.getName());
+console.log(c.getName());
