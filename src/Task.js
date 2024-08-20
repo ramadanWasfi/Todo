2ca9e4bcf3a2030047e5e21191a0a9b8f2f8d67f
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2024-08-20 17:44:45 +0300

    Create Task HTML div element with its style and test it in browser

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { Priority } from "./Priority";
+import "./style.css";
 
 function Task(name = "test", description = "", priorityNumber = 1,
               date = format(new Date(), "d MMM h:m:s a")) {
@@ -25,4 +26,54 @@ function Task(name = "test", description = "", priorityNumber = 1,
             setPriority, getName, setName, getState, setState};
 }
 
-export {Task};
+function createTaskElement(task) {
+        const taskContainer = document.createElement("div");
+        taskContainer.setAttribute("class","taskContainer");
+
+        const taskMainContainer = document.createElement("div");
+        taskMainContainer.setAttribute("class","taskMainContainer");
+
+        const taskDescriptionContainer = document.createElement("div");
+        taskDescriptionContainer.setAttribute("class","taskDescriptionContainer");
+
+        const taskMainContainerLeftPart = document.createElement("div");
+        taskMainContainerLeftPart.setAttribute("class","taskMainContainerLeftPart");
+
+        const taskMainConainerRightPart = document.createElement("div");
+        taskMainConainerRightPart.setAttribute("class","taskMainConainerRightPart");
+
+
+        const taskDateElement = document.createElement('p');
+        taskDateElement.textContent = task.getDate();
+
+        const taskPriorityElement = document.createElement("div");
+        taskPriorityElement.textContent = task.getPriority().getNumber();
+
+        const taskDescriptionElement = document.createElement("label");
+        taskDescriptionElement.textContent = task.getDescription();
+        taskDescriptionElement.setAttribute("for","taskCheckBox");
+        
+
+        const taskCheckBox = document.createElement("input");
+        taskCheckBox.setAttribute("type", "checkbox");
+        taskCheckBox.setAttribute("id","taskCheckBox");
+        taskCheckBox.setAttribute("name","task_state");
+
+        taskMainContainerLeftPart.appendChild(taskCheckBox);
+        taskMainContainerLeftPart.appendChild(taskDescriptionElement);
+
+        taskMainConainerRightPart.appendChild(taskDateElement);
+        taskMainConainerRightPart.appendChild(taskPriorityElement);
+
+        taskMainContainer.appendChild(taskMainContainerLeftPart);
+        taskMainContainer.appendChild(taskMainConainerRightPart);
+
+        taskContainer.appendChild(taskMainContainer);
+
+        return taskContainer;
+}
+
+
+
+
+export {Task, createTaskElement};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import {Task} from "./Task";
+import {Task, createTaskElement} from "./Task";
 import { Todo } from "./Todo";
+import "./style.css";
 
 const task = Task("task A", 'aa',2);
 const task2 = Task("task b", 'd', 1);
 task.setState("finished");
+const taskElement = createTaskElement(task);
+let container = document.querySelector(".container");
+container.appendChild(taskElement);
 
 const todo = Todo("a","b",[task, task2]);
 
