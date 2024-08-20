c770950aa593174c08deefe8b15f637b9c8041aa
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2024-08-20 20:43:31 +0300

    Add CreateProjectElement() to create html project representation

--- src/Project.js
+++ src/Project.js
@@ -1,8 +1,8 @@
-import { Todo } from "./Todo";
+import { Todo , createTodoElement} from "./Todo";
 
-export function Project(name = "test project", description = "nice project", todos = []) {
+function Project(name = "test project", description = "nice project", todos = []) {
     let pname = name;
-    let pdescription = pdescription;
+    let pdescription = description;
     let ptodos = todos;
 
     const getName = () => pname;
@@ -16,6 +16,46 @@ export function Project(name = "test project", description = "nice project", tod
     const setName = (newName) => {pname = newName};
     const setDescription = (newDescription) => {pdescription = newDescription};
     const setTodos = (newTodos) => {ptodos = newTodos};
+
+    return {getName, getDescription, getTodos, getTodoByName, setName, setDescription, setTodos};
 }
 
 
+function createProjectElement(project) {
+    const projectContainer = document.createElement("div");
+    projectContainer.setAttribute("class","projectContainer");
+
+    const projectNameContainer = document.createElement("div");
+    projectNameContainer.setAttribute("class","projectNameContainer");
+
+    const projectDescriptionContainer = document.createElement("div");
+    projectDescriptionContainer.setAttribute("class","projectDescriptionContainer");
+
+    const projectTodosContainer = document.createElement("div");
+    projectTodosContainer.setAttribute("class","projectTodosContainer");
+
+    const todos = project.getTodos();
+    todos.forEach(todo => {
+        const todoElement = createTodoElement(todo);
+        projectTodosContainer.appendChild(todoElement);
+    })
+       
+    
+
+    const projectDescriptionElement = document.createElement('p');
+    projectDescriptionElement.textContent = project.getDescription();
+
+    const projectNameElement = document.createElement('p');
+    projectNameElement.textContent = project.getName();
+
+    projectDescriptionContainer.appendChild(projectDescriptionElement);
+    projectNameContainer.appendChild(projectNameElement);
+
+    projectContainer.appendChild(projectNameContainer);
+    projectContainer.appendChild(projectDescriptionContainer);
+    projectContainer.appendChild(projectTodosContainer);
+
+    return projectContainer;
+}
+
+export {Project, createProjectElement};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import {Task, createTaskElement} from "./Task";
 import { Todo, createTodoElement } from "./Todo";
+import { Project, createProjectElement} from "./Project";
+
 import "./style.css";
 
 const task = Task("task A", 'aa',2);
 const task2 = Task("task b", 'd', 1);
 const task3 = Task("task basda", 'asdasdasad', 4);
 
-task.setState("finished");
-const taskElement = createTaskElement(task);
-let container = document.querySelector(".container");
-container.appendChild(taskElement);
-
 const todo = Todo("health todo","some tasks to prevent you from diseases",[task, task2, task3]);
+const todo2 = Todo("wealth todo","tasks to increase your money freedom",[task, task2, task3]);
+const todo3 = Todo("mental health todo","train your brain to be calm in every situation",[task, task2, task3]);
+
+const project = Project("Human Genome Project", "Hack Human DNA To alter human behavior",[todo, todo2, todo3])
 
 const todoElement = createTodoElement(todo);
-container.appendChild(todoElement);
+const projectElement = createProjectElement(project);
+
+let container = document.querySelector(".container");
+container.appendChild(projectElement);
+
